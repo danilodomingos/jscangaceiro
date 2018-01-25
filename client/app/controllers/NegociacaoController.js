52e744bb4cb046805738e1834dcc42ffcb2fee59
KRYPTON\ddomingos <ddomingos@metatron.com.br>
2018-01-25 10:55:54 -0200

    [Cap 11] - Código do capítulo 11.

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -7,35 +7,55 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        
-        this._negociacoes = 
+
+        this._negociacoes =
             new Bind(new Negociacoes(), new NegociacoesView('#negociacoes'), 'adiciona', 'esvazia');
 
-        this._mensagem 
+        this._mensagem
             = new Bind(new Mensagem(), new MensagemView('#mensagemView'), 'texto');
     }
 
     adiciona(event) {
 
-        event.preventDefault();
-        this._negociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso.';
-        this._limpaFormulario();
+        try {
+            event.preventDefault();
+            this._negociacoes.adiciona(this._criaNegociacao());
+            this._mensagem.texto = 'Negociação adicionada com sucesso.';
+            this._limpaFormulario();
+
+        } catch (error) {
+            console.log(error);
+            console.log(error.stack)
+
+            if(error instanceof DataInvalidaException)
+                this._mensagem.texto = error.message;
+            else
+                this._mensagem.texto = 'Um erro não esperado aconteceu. Entre em contato com o suporte';
+            
+            
+        }
     }
 
-    apaga(){
-        this._negociacoes.esvazia();
-        this._mensagem.texto = 'Negociações apagadas com sucesso.';
+    apaga() {
+
+        try {
+            this._negociacoes.esvazia();
+            this._mensagem.texto = 'Negociações apagadas com sucesso.';  
+
+        } catch (error) {
+            console.log(error);
+            this._mensagem.texto = error.message;
+        } 
     }
 
-    _limpaFormulario() { 
-        this._inputData.value = ''; 
-        this._inputQuantidade.value = 1; 
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
-        this._inputData.focus(); 
+        this._inputData.focus();
     }
 
-    _criaNegociacao(){
+    _criaNegociacao() {
         return new Negociacao(
             DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -11,10 +11,10 @@ class DateConverter{
 
     static paraData(texto){
 
-        if (! /^\d{4}-\d{2}-\d{2}$/.test(texto)) 
-            throw new Error('Deve estar no formato aaaa-mm-dd');
+        if (!/\d{2}\/\d{2}\/\d{4}/.test(texto))
+            throw new DataInvalidaException('Deve estar no formato dd/mm/aaaa');
 
-        return new Date(...texto.split('-')
+        return new Date(...texto.split('/')
                     .map((item, indice) => item - indice % 2));
     }
 }
